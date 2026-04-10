185e901ba72392056ac94790bd546a76573dd7ab
chiyuan5 <1662024152@qq.com>
2026-04-10 19:23:29 +0800

    init: ChiyuanVA main code base

---
+++ app/src/main/java/com/chiyuan/va/app/bean/AppInfo.kt
@@ -0,0 +1,6 @@
+package com.chiyuan.va.app.bean
+
+import android.graphics.drawable.Drawable
+
+
+data class AppInfo(val name:String,val icon:Drawable?,val packageName:String,val sourceDir:String,val isXpModule:Boolean)
